a788277f4f48efe1aa27de49bc85cfa6e10155c6
antonfrancisjeejo <antonfrancisjeejo13@gmail.com>
2022-05-10 23:01:25 +0530

    Initial commit

---
+++ src/components/Banner/Banner.js
@@ -0,0 +1,58 @@
+import axios from "../../helpers/axios";
+import React, { useEffect, useState } from "react";
+import "./Banner.css";
+import requests from "../../helpers/requests";
+
+const Banner = () => {
+  const [movie, setMovie] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const request = await axios.get(requests.fetchNetflixOriginals);
+      // console.log(request);
+      setMovie(
+        request.data.results[
+          Math.floor(Math.random() * request.data.results.length - 1)
+        ]
+      );
+      return request;
+    };
+    fetchData();
+  }, []);
+
+  console.log(movie);
+
+  function truncate(string, n) {
+    return string?.length > n ? string.substr(0, n - 1) + "..." : string;
+  }
+
+  return (
+    <header
+      className="banner"
+      style={{
+        // backgroundImage: `url('https://i.imgur.com/g0jXdZC.jpg')`,
+        // backgroundImage: `url('https://upload.wikimedia.org/wikipedia/commons/thumb/c/cd/Black_flag.svg/1200px-Black_flag.svg.png')`,
+        backgroundImage: `url("https://image.tmdb.org/t/p/original/${movie?.backdrop_path}")`,
+        backgroundSize: "cover",
+        backgroundPosition: "center center",
+      }}
+    >
+      <div className="banner__contents">
+        <h1 className="banner__title">
+          {movie?.title || movie?.name || movie?.original_name}
+        </h1>
+        <div className="banner__buttons">
+          <button className="banner__button">Play</button>
+          <button className="banner__button">My List</button>
+        </div>
+        <h1 className="banner__description">
+          {truncate(movie?.overview, 150)}
+        </h1>
+      </div>
+
+      <div className="banner--fadeBottom" />
+    </header>
+  );
+};
+
+export default Banner;
